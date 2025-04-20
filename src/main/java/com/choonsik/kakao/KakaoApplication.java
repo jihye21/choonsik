1f3b3053e746f7e93f55a02f26f24efd5d4de5ce
jihye21 <112855236+jihye21@users.noreply.github.com>
2025-04-20 17:57:08 +0900

    index setting

--- src/main/java/com/choonsik/kakao/KakaoApplication.java
+++ src/main/java/com/choonsik/kakao/KakaoApplication.java
@@ -2,12 +2,19 @@ package com.choonsik.kakao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class KakaoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KakaoApplication.class, args);
 	}
-
+	
+	@RequestMapping("/")
+	public String index() {
+		return "thymeleaf/index";
+	}
 }
